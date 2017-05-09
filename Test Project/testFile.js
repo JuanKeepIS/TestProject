fecd96cba6c1212b7bd4a1b1b48ff2fdea7c6f58
Julti <jota.ch2010@gmail.com>
2017-05-09 17:46:52 -0500

    Test

---
+++ Test Project/testFile.js
@@ -0,0 +1,18 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       09 Jan 2015     Mitchell
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @param {String} type Operation types: create, edit, view, copy, print, email
+ * @param {nlobjForm} form Current form
+ * @param {nlobjRequest} request Request object
+ * @returns {Void}
+ */
+
